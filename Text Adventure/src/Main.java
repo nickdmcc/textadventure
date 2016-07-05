5a41f0df198f4a5b58ff197ccda5490ecf978688
soysaucecrakr <mccartynd@appstate.edu>
2016-07-05 10:13:04 -0700

    Added swing functionality and changed input to buttons

--- Text Adventure/src/Main.java
+++ Text Adventure/src/Main.java
@@ -1,10 +1,25 @@
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.io.FileReader;
 import java.util.Scanner;
-public class Main {
-	
-	static private int STILL_PLAYING = 1;
-	static private int QUIT = 0;
+import java.awt.event.*;
+
+public class Main extends JFrame{
+
 	static private String FILE = "World.txt";
+	
+	static Room room = new Room();
+		
+	JButton button1;
+	JButton button2;
+	JButton button3;
+	JButton button4;
+	static JLabel label1;
+	static JTextArea textArea1;
 	
 	/**
 	 * Shows the room description after typing "look" or when entering a new room
@@ -12,8 +27,7 @@ public class Main {
 	 */
 	public static void displayRoom(Room room)
 	{
-		System.out.println(room.getRoomDescription());
-		System.out.println();
+		textArea1.setText(room.getRoomDescription());
 	}
 
 	public static void getRoomInfo(String file, Room room)
@@ -72,10 +86,19 @@ public class Main {
 	
 	public static void move(Room room, String strRoom)
 	{
-		if (strRoom.equals("None"))
+		if ((room.getRoomEast().equals("None") && room.getRoomNorth().equals("None") && room.getRoomSouth().equals("None") && room.getRoomWest().equals("None")))
 		{
-			System.out.println("You can't go that way!");
+			label1.setText("Game Over");
 			return;
+		}
+		else if (strRoom.equals("None"))
+		{
+			label1.setText("You can't go that way!");
+			return;
+		}
+		else
+		{
+			label1.setText("");
 		}
 		
 		room.setCurrentRoom(strRoom);
@@ -84,72 +107,115 @@ public class Main {
 		displayRoom(room);
 	}
 	
-	public static int getInput(Room room, String input)
+	public Main()
 	{
-		if (input.equals("look"))
-		{
-			displayRoom(room);
-		}
-		else if (input.equals("quit"))
-		{
-			System.out.println("Game Over");
-			return QUIT;
-		}
-		else if (input.equals("north"))
-		{
-			move(room, room.getRoomNorth());
-		}
-		else if (input.equals("east"))
-		{
-			move(room, room.getRoomEast());
-		}
-		else if (input.equals("south"))
-		{
-			move(room, room.getRoomSouth());
-		}
-		else if (input.equals("west"))
-		{
-			move(room, room.getRoomWest());
-		}
-		else
-		{
-			System.out.println("Commands: look north south east west quit");
-		}
+		this.setSize(600,300);
 
-		return STILL_PLAYING;
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		
+		Dimension dim = tk.getScreenSize();
+		
+		int xPos = (dim.width / 2) - (this.getWidth() / 2);
+		int yPos = (dim.height / 2) - (this.getHeight() / 2);
+		
+		this.setLocation(xPos, yPos);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setTitle("Text Adventure");
+		
+		JPanel textPanel = new JPanel();
+		textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.PAGE_AXIS));
+		
+		textArea1 = new JTextArea();
+		textArea1.setLineWrap(true);
+		textArea1.setWrapStyleWord(true);
+		textArea1.setEditable(false);
+		textPanel.add(textArea1);
+		
+		JPanel labelPanel = new JPanel();
+		labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.LINE_AXIS));
+		label1 = new JLabel("");
+		labelPanel.add(label1);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
+		
+		button1 = new JButton("North");
+		ListenForButton lForButtonN = new ListenForButton();
+		
+		button1.addActionListener(lForButtonN);
+		buttonPanel.add(button1);
+		
+		button2 = new JButton("East");
+		ListenForButton lForButtonE = new ListenForButton();
+		
+		button2.addActionListener(lForButtonE);
+		buttonPanel.add(button2);
+		
+		button3 = new JButton("South");
+		ListenForButton lForButtonS = new ListenForButton();
+		
+		button3.addActionListener(lForButtonS);
+		buttonPanel.add(button3);
+		
+		button4 = new JButton("West");
+		ListenForButton lForButtonW = new ListenForButton();
+		
+		button4.addActionListener(lForButtonW);
+		buttonPanel.add(button4);
+		
+		Container contentPane = getContentPane();
+		contentPane.add(textPanel, BorderLayout.NORTH);
+		contentPane.add(labelPanel, BorderLayout.CENTER);
+		contentPane.add(buttonPanel, BorderLayout.PAGE_END);
+		
+		this.setVisible(true);
 	}
 	
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
-		Room room = new Room();
-		String strInput = "";
+		new Main();
 		try{
 			Scanner scan = new Scanner(new FileReader(FILE));
-			Scanner reader = new Scanner(System.in);
 			scan.next();
 			room.setCurrentRoom(scan.next());
 			getRoomInfo(FILE, room);
 			displayRoom(room);
-			while(true)
-			{
-				if ((room.getRoomEast().equals("None") && room.getRoomNorth().equals("None") && room.getRoomSouth().equals("None") && room.getRoomWest().equals("None")))
-				{
-					System.out.println("Game Over");
-					break;
-				}
-				System.out.println();
-				System.out.print(": ");
-				strInput = reader.next();
-				if (getInput(room, strInput) == QUIT)
-				{
-					break;
-				}
-			}
 			scan.close();
-			reader.close();
 		}catch (Exception e){//Catch exception if any
 		      System.err.println("Error: " + e.getMessage());
 		}
 		
+	}
+	
+	private class ListenForButton implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			if (e.getSource() == button1)
+			{
+				move(room, room.getRoomNorth());
+			}
+			else if (e.getSource() == button2)
+			{
+				move(room, room.getRoomEast());
+
+			}
+			else if (e.getSource() == button3)
+			{
+				move(room, room.getRoomSouth());
+
+			}
+			else if (e.getSource() == button4)
+			{
+				move(room, room.getRoomWest());
+
+			}
+			else
+			{
+				
+			}
+		}
 	}
 }
